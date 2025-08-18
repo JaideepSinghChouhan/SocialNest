8eca587ae56518fdf0d627639ea52d875e7d14d0
JaideepSinghChouhan <jd8769023@gmail.com>
2025-08-18 21:56:22 +0530

    fixes

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,13 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoute = () => {
-  const { user, loading } = useAuth();
-  
-  if (loading) {
-    return <div>Loading...</div>; // Or your loading component
-  }
-  
+  const user = JSON.parse(localStorage.getItem("user"));
   return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
